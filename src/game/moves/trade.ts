7f766120124589e43fa10f4d3e0fde1f34651371
brisberg <brandon.risberg@gmail.com>
2021-06-25 14:44:23 -0700

    Strongly typed Trade and Travel actions

--- src/game/moves/trade.ts
+++ src/game/moves/trade.ts
@@ -1,28 +1,28 @@
-import type {Ctx, MoveFn} from 'boardgame.io';
+import type {Ctx} from 'boardgame.io';
 import {INVALID_MOVE} from 'boardgame.io/core';
 import type {Color, GameState} from '../state';
 
-export const trade: MoveFn<GameState, Ctx> =
-    (G, ctx, shipId: string, color: Color) => {
-      const ship = G.ships[shipId];
+export function trade(
+    G: GameState, ctx: Ctx, shipId: string, color: Color): string {
+  const ship = G.ships[shipId];
 
-      if (!ship || ship.player !== ctx.currentPlayer) {
-        return INVALID_MOVE;
-      }
+  if (!ship || ship.player !== ctx.currentPlayer) {
+    return INVALID_MOVE;
+  }
 
-      if (ship.piece.color === color) {
-        return INVALID_MOVE;
-      }
+  if (ship.piece.color === color) {
+    return INVALID_MOVE;
+  }
 
-      const size = ship.piece.size;
+  const size = ship.piece.size;
 
-      if (G.bank[color][size] <= 0) {
-        return INVALID_MOVE;
-      }
+  if (G.bank[color][size] <= 0) {
+    return INVALID_MOVE;
+  }
 
-      // TODO: Implement checking for power access
+  // TODO: Implement checking for power access
 
-      G.bank[color][size] -= 1;
-      G.bank[ship.piece.color][size] += 1;
-      ship.piece.color = color;
-    };
+  G.bank[color][size] -= 1;
+  G.bank[ship.piece.color][size] += 1;
+  ship.piece.color = color;
+}
--- src/game/moves/travel.ts
+++ src/game/moves/travel.ts
@@ -1,61 +1,62 @@
-import type {Ctx, MoveFn} from 'boardgame.io';
+import type {Ctx} from 'boardgame.io';
 import {INVALID_MOVE} from 'boardgame.io/core';
 import type {GameState, Piece, StarSystem} from '../state';
 
-export const travel: MoveFn<GameState, Ctx> =
-    (G, ctx, shipId: string, destStarId: string, newPiece?: Piece) => {
-      const ship = G.ships[shipId];
+export function travel(
+    G: GameState, ctx: Ctx, shipId: string, destStarId: string,
+    newPiece?: Piece): string {
+  const ship = G.ships[shipId];
 
-      if (!ship || ship.player !== ctx.currentPlayer) {
-        return INVALID_MOVE;
-      }
+  if (!ship || ship.player !== ctx.currentPlayer) {
+    return INVALID_MOVE;
+  }
 
-      const star = G.stars[ship.location];
-      let dest = G.stars[destStarId];
+  const star = G.stars[ship.location];
+  let dest = G.stars[destStarId];
 
-      if (dest && newPiece) {
-        console.warn(
-            'Traveling to an existing star should not specify a new Piece ' +
-            'for the star. Did you mean to specify a new Star ID?');
-      }
+  if (dest && newPiece) {
+    console.warn(
+        'Traveling to an existing star should not specify a new Piece ' +
+        'for the star. Did you mean to specify a new Star ID?');
+  }
 
-      if (!dest) {
-        if (!newPiece) {
-          console.error(
-              'Traveling to discover a new Star must specify ' +
-              'the Piece to use for it');
-          return INVALID_MOVE;
-        }
+  if (!dest) {
+    if (!newPiece) {
+      console.error(
+          'Traveling to discover a new Star must specify ' +
+          'the Piece to use for it');
+      return INVALID_MOVE;
+    }
 
-        if (G.bank[newPiece.color][newPiece.size - 1] <= 0) {
-          console.error(`No ${newPiece.color}:${
-              newPiece.size} pieces remaining to create a new star.`);
-          return INVALID_MOVE;
-        }
+    if (G.bank[newPiece.color][newPiece.size - 1] <= 0) {
+      console.error(`No ${newPiece.color}:${
+          newPiece.size} pieces remaining to create a new star.`);
+      return INVALID_MOVE;
+    }
 
-        // Build the newly discovered star
-        dest = {id: destStarId, piece: newPiece};
+    // Build the newly discovered star
+    dest = {id: destStarId, piece: newPiece};
 
-        if (!isConnected(star, dest)) {
-          console.error(
-              'Cannot discover a new Star the same size as the origin Star.');
-          return INVALID_MOVE;
-        }
+    if (!isConnected(star, dest)) {
+      console.error(
+          'Cannot discover a new Star the same size as the origin Star.');
+      return INVALID_MOVE;
+    }
 
-        G.stars[destStarId] = dest;
-        G.bank[newPiece.color][newPiece.size - 1]--;
-      }
+    G.stars[destStarId] = dest;
+    G.bank[newPiece.color][newPiece.size - 1]--;
+  }
 
-      if (!isConnected(star, dest)) {
-        console.error(
-            'Cannot travel to a new Star the same size as the origin Star.');
-        return INVALID_MOVE;
-      }
+  if (!isConnected(star, dest)) {
+    console.error(
+        'Cannot travel to a new Star the same size as the origin Star.');
+    return INVALID_MOVE;
+  }
 
-      // TODO: Implement checking for power access
+  // TODO: Implement checking for power access
 
-      ship.location = dest.id;
-    };
+  ship.location = dest.id;
+}
 
 function isConnected(starA: StarSystem, starB: StarSystem): boolean {
   return starA.piece.size !== starB.piece.size;
